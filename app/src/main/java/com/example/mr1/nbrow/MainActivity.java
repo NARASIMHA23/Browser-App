7a29f3c01ffc51c88deb2d22f7363219769c89da
Nandakishore <andab@unifytech.com>
2018-02-07 20:47:00 +0530

    second commit

---
+++ app/src/main/java/com/example/mr1/nbrow/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.mr1.nbrow;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText=(EditText)findViewById(R.id.editText);
+        button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(Intent.ACTION_VIEW);
+                in.setData(Uri.parse(editText.getText().toString()));
+                startActivity(in);
+
+            }
+        });
+    }
+}
+
